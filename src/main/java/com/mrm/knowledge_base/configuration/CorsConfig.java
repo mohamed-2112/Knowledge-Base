88182612e4bf4efc9b0823cc0dae8eebdad2e0ab
Mohamed Rasmy Fathy Mohamed <67447434+mohamed-2112@users.noreply.github.com>
2026-06-02 22:20:40 +0000

    dockarizing the backend

--- src/main/java/com/mrm/knowledge_base/configuration/CorsConfig.java
+++ src/main/java/com/mrm/knowledge_base/configuration/CorsConfig.java
@@ -5,16 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.List;
 
 @Configuration
 public class CorsConfig {
+    @Value("${app.frontend.origin}")
+    private String frontendOrigin;
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000"));
+        corsConfiguration.setAllowedOrigins(List.of(frontendOrigin));
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         corsConfiguration.setAllowedHeaders(List.of("Content-Type", "X-XSRF-TOKEN"));
         corsConfiguration.setAllowCredentials(true);
